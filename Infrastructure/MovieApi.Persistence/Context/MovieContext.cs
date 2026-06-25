df6d45dd4275c11483139409aff0b9c29abf8df5
Gülendamcetin <gulendamcetin970@gmail.com>
2026-06-25 16:23:11 +0300

    Onion architecture was set up, and entities were created.

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-3K46L5U\\SQLEXPRESS01; inital Catalog=MovieApiDb;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Movie> Movies{ get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
